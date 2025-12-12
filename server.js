d132e136e7146611a1629b37f7266196d76dcc43
Jhonatas Galdino <jhonatasgrs@gmail.com>
2025-12-12 11:19:36 -0300

    Update server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import routes from "./src/routes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", routes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`API rodando em http://localhost:${PORT}`);
+});

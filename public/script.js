cc6f67692ea954b96d9aaf95ce5b491d11aedd8f
Jhonatas Galdino <jhonatasgrs@gmail.com>
2025-12-12 12:22:37 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 async function roll() {
     const expr = document.getElementById("expr").value;
 
-    const res = await fetch(`http://localhost:3000/api/roll?expr=${expr}`);
+    const res = await fetch(`/api/roll?expr=${expr}`)
     const data = await res.json();
 
     document.getElementById("output").textContent = JSON.stringify(
